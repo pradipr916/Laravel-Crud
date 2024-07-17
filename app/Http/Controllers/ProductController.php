d0d973ce4e5228aeb473033e187b027e8f6de91d
Raghwan Pradeep Narayan <pradipr916@gmail.com>
2024-07-17 11:52:55 +0530

    Database configured and controller created.

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //Shows product page
+    public function index()
+    {
+
+    }
+    //Shows create page
+    public function create(){
+
+    }
+    //Store product in db
+    public function store(){
+
+    }
+    //Show product edit page
+    public function edit(){
+
+    }
+    //Update product in db
+    public function update(){
+
+    }
+    //Deletes product from db
+    public function destroy(){
+
+    }
+
+}
